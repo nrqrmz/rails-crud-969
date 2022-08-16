a656dcb9cc1b1db118a572f99350984182bde9aa
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-16 10:34:29 -0500

    add links to improve user experience

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   # code in silo
 
   get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
   post 'restaurants', to: 'restaurants#create'
   get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-  get 'restaurants/:id/edit', to: 'restaurants#edit'
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
   patch 'restaurants/:id', to: 'restaurants#update'
   delete 'restaurants/:id', to: 'restaurants#destroy'
 end
