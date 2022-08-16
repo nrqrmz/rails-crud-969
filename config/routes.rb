46629afb7d96a94e7c96a44863382e54ede25ef5
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-16 10:16:47 -0500

    add routes/controller for #create

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'restaurants', to: 'restaurants#index'
   get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
   get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
 
   # post
